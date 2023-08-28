e541f5fd7fb94ccb18220addc767a4e446620635
Hamza Kamelen <hamzakamelen12345@gmail.com>
2023-08-28 23:12:56 +0500

    Conti...

--- Navigation/Tabs.tsx
+++ Navigation/Tabs.tsx
@@ -8,120 +8,131 @@ import Notification from '../Screens/Notification';
 import { NavigationContainer } from '@react-navigation/native';
 import styles from '../style/style';
 import HMIcon from '../Components/Icons';
-import { Text, View } from 'react-native';
-function TabNavigation() {
+import { Text, TouchableOpacity, View } from 'react-native';
+function TabNavigation({navigation}:any) {
     const Tab = createBottomTabNavigator();
     return (
-            <Tab.Navigator
-                screenOptions={{
-                    tabBarActiveBackgroundColor: "#343a60",
-                    tabBarShowLabel: false,
-                    headerShown: false,
-                    tabBarHideOnKeyboard: true,
-                    tabBarStyle: {
-                        position: 'relative',
-                        backgroundColor: "#343a40",
-                        borderColor: 'white',
-                        left: 2,
-                        right: 2,
-                        bottom:1,
-                        borderRadius: 10,
-                        elevation: 0,
-                        height: 63,
-                        ...styles.shadow3,
+        <Tab.Navigator
+        initialRouteName='Post'
+            screenOptions={{
+                tabBarActiveBackgroundColor: "#343a60",
+                tabBarShowLabel: false,
+                headerShown: false,
+                tabBarHideOnKeyboard: true,
+                tabBarStyle: {
+                    position: 'relative',
+                    backgroundColor: "#343a40",
+                    borderColor: 'white',
+                    left: 2,
+                    right: 2,
+                    bottom: 1,
+                    borderRadius: 10,
+                    elevation: 0,
+                    height: 63,
+                    ...styles.shadow3,
 
-                    }
+                }
+            }}
+        >
+            <Tab.Screen name="Homes" component={Home}
+                options={{
+                    tabBarIcon: () => (
+                        <View>
+                            <View style={[{ marginLeft: 3 }]}>
+                                <HMIcon name='home' size={30} color="grey" />
+                            </View>
+                            <Text>Home</Text>
+                        </View>
+                    )
                 }}
-            >
-                <Tab.Screen name="Homes" component={Home}
-                    options={{
-                        tabBarIcon: () => (
-                            <View>
-                                <View style={[{ marginLeft: 3 }]}>
-                                    <HMIcon name='home' size={30} color="grey" />
-                                </View>
-                                <Text>Home</Text>
+            />
+            <Tab.Screen name="Search" component={Search}
+                options={{
+                    tabBarIcon: () => (
+                        <View>
+                            <View style={{ marginLeft: 5 }}>
+                                <HMIcon name='search' size={30} color="grey" />
                             </View>
-                        )
-                    }}
-                />
-                <Tab.Screen name="Search" component={Search}
-                    options={{
-                        tabBarIcon: () => (
-                            <View>
-                                <View style={{ marginLeft: 5 }}>
-                                    <HMIcon name='search' size={30} color="grey" />
-                                </View>
-                                <Text>Search</Text>
+                            <Text>Search</Text>
+                        </View>
+                    )
+                }}
+            />
+            <Tab.Screen name="Post" component={Post}
+                options={{
+                    tabBarActiveBackgroundColor: "#343a35",
+                    tabBarStyle: {
+                        display: "none"
+                    },
+                    headerShown: true,
+                    headerTitleAlign:"center",
+                    headerTitleStyle:{
+                        color:"white",
+                        fontSize:25
+                    },
+                    headerStyle: {
+                       backgroundColor: "black",
+                    },
+                    headerLeft:()=>(
+                        <TouchableOpacity  style={[styles.ms1]} onPress={()=>(navigation.navigate('Homes'))} >
+                        <HMIcon name="arrow-back-ios-new" color="white" size={32} />
+                        </TouchableOpacity>
+                    ),
+                    tabBarIcon: () => (
+                        <View style={[{ marginTop: -8 }]}>
+                            <View style={[styles.bgDanger, styles.roundedPill, { marginLeft: -4 }]}>
+                                <HMIcon name='add' size={45} color="white" />
                             </View>
-                        )
-                    }}
-                />
-                <Tab.Screen name="Post" component={Post}
-                    options={{
-                        tabBarActiveBackgroundColor: "#343a35",
-                        tabBarStyle:{
-                            display:"none"
-                        },
-                        headerShown:true,
-                        headerStyle:{
-                            backgroundColor:"blue"
-                        },
-                        tabBarIcon: () => (
-                            <View style={[{ marginTop: -8 }]}>
-                                <View style={[styles.bgDanger, styles.roundedPill, { marginLeft: -4 }]}>
-                                    <HMIcon name='add' size={45} color="white" />
-                                </View>
+                        </View>
+                    )
+                }}
+            />
+            <Tab.Screen name="Notification" component={Notification}
+                options={{
+                    tabBarIcon: () => (
+                        <View>
+                            <View style={{ marginLeft: 8 }}>
+                                <HMIcon name='favorite-outline' size={30} color="grey" />
                             </View>
-                        )
-                    }}
-                />
-                <Tab.Screen name="Notification" component={Notification}
-                    options={{
-                        tabBarIcon: () => (
-                            <View>
-                                <View style={{ marginLeft: 8 }}>
-                                    <HMIcon name='favorite-outline' size={30} color="grey" />
-                                </View>
-                                <Text>Activity</Text>
-                            </View>
-                        )
-                    }} />
-                <Tab.Screen name="Profile" component={Profile}
-                    options={{
-                        headerShown: true,
-                        headerTitleStyle: {
-                            display: "none"
-                        },
-                        headerLeft: () => (
-                            <View style={[styles.p1]}>
-                                <HMIcon name="language" size={30} color="white" />
-                            </View>
-                        ),
-                        headerRight: () => (
-                            <View style={[styles.flexRow, styles.justifyContentAround]}>
-                                <View style={[styles.me1]}>
+                            <Text>Activity</Text>
+                        </View>
+                    )
+                }} />
+            <Tab.Screen name="Profile" component={Profile}
+                options={{
+                    headerShown: true,
+                    headerTitleStyle: {
+                        display: "none"
+                    },
+                    headerLeft: () => (
+                        <View style={[styles.p1]}>
+                            <HMIcon name="language" size={30} color="white" />
+                        </View>
+                    ),
+                    headerRight: () => (
+                        <View style={[styles.flexRow, styles.justifyContentAround]}>
+                            <View style={[styles.me1]}>
                                 <HMIcon name="diversity-3" size={35} color="white" />
-                                </View>
-                                <View style={[styles.me1]}>
+                            </View>
+                            <View style={[styles.me1]}>
                                 <HMIcon name="drag-handle" size={35} color="white" />
-                                </View>
                             </View>
-                        ),
-                        headerStyle: {
-                            backgroundColor: "#343a40",
-                            shadowColor:"white"
-                        },
-                        tabBarIcon: () => (
-                            <View>
-                                <View style={{ marginLeft: 5 }}>
-                                    <HMIcon name='person-outline' size={30} color="grey" />
-                                </View>
-                                <Text>Profile</Text>
+                        </View>
+                    ),
+                    headerStyle: {
+                        backgroundColor: "#343a40",
+                        shadowColor: "white"
+                    },
+                    tabBarIcon: () => (
+                        <View>
+                            <View style={{ marginLeft: 5 }}>
+                                <HMIcon name='person-outline' size={30} color="grey" />
                             </View>
-                        )
-                    }} />
-            </Tab.Navigator>
+                            <Text>Profile</Text>
+                        </View>
+                    )
+                }} />
+        </Tab.Navigator>
     )
 }
 export default TabNavigation;
--- Screens/Post.tsx
+++ Screens/Post.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import { Image, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Image, Text, TextInput, View } from 'react-native'
 import styles from '../style/style'
+import HMIcon from '../Components/Icons'
 
 const Post = () => {
+  const [avatar, setavatar] = useState('')
+  // const uploadImage = () => {
+  //   ImagePicker.openPicker({
+  //     width: 300,
+  //     height: 300,
+  //     cropping: true,
+  //     compressImageQuality: 0.9,
+  //     includeBase64: true,
+  //   }).then((image: ImageOrVideo | any) => {
+  //     if(image){
+  //     setavatar('data:image/jpeg;base64,' + image?.data)
+  //     }
+  //   })
+  // }
   return (
     <View style={[styles.bgLightBlack, styles.h100, styles.p2]}>
       {/* <Text style={[styles.fs2, styles.textBold, styles.textLight]}>Post</Text> */}
-      <Image
-        style={[
-          {
-            height: 80,
-            width: 80,
-            borderRadius: 50,
-          },
-        ]}
-        source={{ uri: "https://scontent.fkhi8-1.fna.fbcdn.net/v/t1.6435-9/86794394_787989285021157_3379173860678041600_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_eui2=AeH5PO1Hv2hq5xCorItaTnX759OQtX360Rrn05C1ffrRGooupLep2ILAvPm55BpVxbUGo7QMek4ws2ORHmWLkVip&_nc_ohc=a8nMk1wP4HgAX8ig7Dj&_nc_ht=scontent.fkhi8-1.fna&oh=00_AfC5VJ9cqNLYYYx9oKiSxlTfFj_9XrKOtcTvE2vfgiTqwA&oe=6509D165"  }}
-      />
+      <View style={[styles.flexRow]}>
+        <Image
+          style={[
+            {
+              height: 80,
+              width: 80,
+              borderRadius: 50,
+            },
+          ]}
+          source={{ uri: "https://scontent.fkhi8-1.fna.fbcdn.net/v/t1.6435-9/86794394_787989285021157_3379173860678041600_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_eui2=AeH5PO1Hv2hq5xCorItaTnX759OQtX360Rrn05C1ffrRGooupLep2ILAvPm55BpVxbUGo7QMek4ws2ORHmWLkVip&_nc_ohc=a8nMk1wP4HgAX8ig7Dj&_nc_ht=scontent.fkhi8-1.fna&oh=00_AfC5VJ9cqNLYYYx9oKiSxlTfFj_9XrKOtcTvE2vfgiTqwA&oe=6509D165" }}
+        />
+        <Text style={[styles.fs4, styles.ms1, styles.mt1, styles.textBold, styles.textWhite]}>Hamza Kamelen</Text>
+      </View>
+      <Text style={[{ marginLeft: 93, marginTop: -40 }]}>hamzakamelen_2244</Text>
+      <TextInput style={[styles.mt3, styles.border1, styles.p1, styles.borderLight, { borderRadius: 5 }]} multiline placeholder='Write Something '></TextInput>
+      <Image style={{ height: 90, width: 90, marginLeft: 10,borderRadius: 5, }} source={{ uri: avatar ? avatar : 'https://lh6.googleusercontent.com/proxy/xJTqeKdDUTpCQIdSDgSC-zkek2K7N2ziuCHcASBiROTcdMwCn15OH1080m1_R_EScPMzIbnOeCSN2DBGgLE0AqZgRmVgw9KM-L9OD-KeVvN-YM00k65q=s0-d' }} />
+          <View style={styles.flexRow}>
+            {/* <HMIcon name="cloud-upload" color="grey" size={27} /> */}
+            <Text style={[styles.textBlack, { marginTop: 2, marginLeft: 5 }, styles.fs5, styles.mb2]}>Upload/Change Picture</Text>
+          </View>
     </View>
   )
 }
